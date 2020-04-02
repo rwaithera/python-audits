df6e5807156c14543192b51080d7a31327e246f1
Ruth Waithera <rwaithera@Ruths-MacBook-Pro.local>
2020-04-02 18:55:26 +0300

    refactor test cases into classes

--- ch-2/test_sum.py
+++ ch-2/test_sum.py
@@ -1,11 +1,12 @@
 from src.sum import sum
 
-def test_multiple_positive_numbers():
-    assert sum(5, 2, 3) == 10
+class TestSum:
+    def test_multiple_positive_numbers(self):
+        assert sum(5, 2, 3) == 10
 
-def test_multiple_negative_numbers():
-    assert sum(-15, -7, -3) == -25
+    def test_multiple_negative_numbers(self):
+        assert sum(-15, -7, -3) == -25
 
-def test_multiple_positive_and_negative_numbers():
-    assert sum(-15, 7, 3) == -5
-    assert sum(10, -5, 2, -11, 10) == 6
+    def test_multiple_positive_and_negative_numbers(self):
+        assert sum(-15, 7, 3) == -5
+        assert sum(10, -5, 2, -11, 10) == 6
